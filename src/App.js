b254490cd2def8e542384d8ae153943a44312270
hiro9108 <hiroshi.8.egawa@gmail.com>
2021-03-22 09:50:48 -0700

    change: app.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import React from 'react'
-import ToDoList from './components/ToDoList'
-import './styles.css'
-import { connect } from 'react-redux'
+import React from "react";
+import ToDoList from "./components/ToDoList";
+import "./styles.css";
+import { useSelector } from "react-redux";
 
-function App({ toDoList }) {
+function App() {
+  const toDoList = useSelector((state) => state.toDoList);
+
   return (
-    <div className='container'>
-      <h1 className='center blue-text'>ToDo's ({toDoList.length})</h1>
+    <div className="container">
+      <h1 className="center blue-text">ToDo's ({toDoList.length})</h1>
       <ToDoList />
     </div>
-  )
+  );
 }
 
-const mapStateToProps = (state) => {
-  // console.log(state)
-  return {
-    toDoList: state.toDoList,
-  }
-}
+// const mapStateToProps = (state) => {
+//   // console.log(state)
+//   return {
+//     toDoList: state.toDoList,
+//   };
+// };
 
-export default connect(mapStateToProps)(App)
+export default App;
